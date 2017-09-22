13982aaca631ffe76974d824040cd5372a1a9ac4
Brandon Fairburn <fairgburn@gmail.com>
2017-09-22 03:47:01 -0400

    add records is working

--- DotNetCppWrapper/src/wrapper/ManagedFoxProBuffer.cpp
+++ DotNetCppWrapper/src/wrapper/ManagedFoxProBuffer.cpp
@@ -5,8 +5,9 @@
 #include "ManagedFoxProBuffer.h"
 #include "ManagedFoxProRecord.h"
 
+#using "AcsLib.NET.dll"
+
 using namespace System;
-using namespace AcsLib::FoxPro;
 using namespace AcsNetLib::FoxPro;
 using ManagedWrappers::ManagedFoxProBuffer;
 using ManagedWrappers::ManagedFoxProRecord;
@@ -61,7 +62,7 @@ Public
 // protected contructor/destructor 
 // force creation by CreateBuffer() since the constructor takes a non-native type as a parameter
 //
-ManagedFoxProBuffer::ManagedFoxProBuffer(FoxProBuffer^ buf) : _buffer(buf)
+ManagedFoxProBuffer::ManagedFoxProBuffer(CSNS::FoxProBuffer^ buf) : _buffer(buf)
 {
 	// store a wrapper for each C# record
 	for each (auto rec in _buffer->Records) {
@@ -99,63 +100,10 @@ ManagedFoxProBuffer::~ManagedFoxProBuffer()
 		delete rp;
 	}
 
-	// clean the record wrappers
-	/*if (_records != nullptr)
-	{
-		// deallocate the individual pointers
-		for (int i = 0; i < _records_size; i++) {
-			ManagedRecordPtr rec = _records[i];
-			if (rec != nullptr)
-				delete rec;
-		}
-
-		// deallocate array
-		delete[] _records;
-	}*/
-
 	// buffer is always dynamically allocated, so delete it too
 	delete this;
 }
 //-------------------------------------------------------------------------------
-
-
-/*--------------------
-* private methods
-*--------------------*/
-
-// grow record pointer array (powers of 2)
-/*void ManagedFoxProBuffer::GrowRecordPtrArray()
-{
-	if (_records == nullptr) return;
-
-	// double array size and allocate memory for larger array
-	int new_size = _records_size << 1;
-	ManagedRecordPtr* new_array = new ManagedRecordPtr[new_size];
-
-	// store existing pointers, set extra elements to nullptr
-	for (int i = 0; i < new_size; i++) {
-		new_array[i] = (i < _records_size) ? _records[i] : nullptr;
-	}
-
-	// move _records pointer to the new array, update _records_size
-	delete[] _records;
-	_records = new_array;
-	_records_size = new_size;
-}
-
-// shrink record pointer array (powers of 2)
-// can be dangerous - calling this when less than half the array is nullptr would cause a memory leak
-void ManagedFoxProBuffer::ShrinkRecordPtrArray()
-{
-	if (_records == nullptr) return;
-
-	// opposite of above algorithm
-	int new_size = _records_size >> 1;
-	auto new_array = new ManagedRecordPtr[new_size];
-
-
-
-}*/
 
 
 /*------------------------------------------------------------------------------
@@ -165,7 +113,7 @@ void ManagedFoxProBuffer::ShrinkRecordPtrArray()
 // use factory model to create instances
 CFoxProBuffer* ManagedFoxProBuffer::CreateBuffer(char* dbfFile)
 {
-    auto net_buffer = gcnew FoxProBuffer(gcnew String(dbfFile));
+    auto net_buffer = gcnew CSNS::FoxProBuffer(gcnew String(dbfFile));
     return new ManagedFoxProBuffer(net_buffer);
 }
 
@@ -192,24 +140,11 @@ void ManagedFoxProBuffer::SaveAs(char* outputFile)
 IRecordPtr ManagedFoxProBuffer::GetRecord(int index)
 {
 	return _vectRecords[index];
-
-	// old way: create new record wrapper, let destructor handle deallocation when
-	//   it goes out of scope in client code
-	//
-	// return new ManagedFoxProRecord(_buffer->GetRecord(index));
-	//   - have to change so client can hold/change a persistent pointer to a record
-	//
-	// new way: wrapper performs and tracks all memory allocations internally
-	
-	//return _records[index];
-
 }
 
 IRecordPtr ManagedFoxProBuffer::operator[] (int index)
 {
 	return this->GetRecord(index);
-	//IRecord* rec = this->GetRecord(index);
-	//return rec;
 }
 
 void ManagedFoxProBuffer::AddRecord(IRecordPtr record)
@@ -223,12 +158,6 @@ void ManagedFoxProBuffer::AddRecord(IRecordPtr record)
 
 	// also update the wrapper records 
 	_vectRecords.push_back(rec_wrapper);
-
-    // cast the native record interface to managed type, get the C# record from it,
-    //  then add the record to C#'s buffer
-    /*auto mfpr = (ManagedRecordPtr)record;
-    auto csrec = mfpr->GetCSRecord();
-    _buffer->AddRecord(csrec);*/
 }
 
 void ManagedFoxProBuffer::RemoveRecord(int index)
@@ -238,7 +167,7 @@ void ManagedFoxProBuffer::RemoveRecord(int index)
 
 IRecordPtr ManagedFoxProBuffer::CreateNewRecord()
 {
-    Record^ new_record = _buffer->CreateNewRecord(' ');
+    CSNS::Record^ new_record = _buffer->CreateNewRecord(' ');
            
     return new ManagedFoxProRecord(new_record);
 }
